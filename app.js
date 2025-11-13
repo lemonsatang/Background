b155e69df7a7591ecc8a38ffb6bd2d2a7dc1c3f0
JMS <jjjj9336@naver.com>
2025-11-13 14:56:09 +0900

    최초커밋

---
+++ app.js
@@ -0,0 +1,15 @@
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.querySelector("body");
+const h3 = document.querySelector("h3");
+
+// 이벤트 색을 바꿨을때
+color1.addEventListener("input", myColor);
+color2.addEventListener("input", myColor);
+
+// 실행 함수
+function myColor() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  h3.textContent = body.style.background + ";";
+}
